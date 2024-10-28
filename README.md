426b68a2fda3e91ea869634eeac35b833d4f1c22
araxia555 <raymund.herradura.nz@gmail.com>
2024-10-28 18:53:48 +1300

    initial commit resume using astro

---
+++ README.md
@@ -0,0 +1,11 @@
+This is inspired by the AWS Cloud Resume Challenge.
+The AWS services used to build this site are the following.
+- HTML & CSS with Astro & Tailwind CSS
+- S3, Cloudfront, Route53, 
+- IAM Identity Center and Organizations
+- HTTPS
+- DNS
+- Javascript
+- Database
+- API: Python & Lambda
+- Terraform
---
+++ tailwind.config.cjs
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: [
+          'Cascadia Code',
+          'Fira Code',
+          'Fira Mono',
+          'Ubuntu Mono',
+          'ui-monospace',
+          'Courier New',
+          'monospace',
+        ],
+        sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  darkMode: 'selector',
+  plugins: [],
+};
